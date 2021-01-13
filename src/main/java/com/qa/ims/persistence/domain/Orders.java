75e3480f5088a10a72c9042f046e9d13c8e1284a
AUraihQA <AUraih@qa.com>
2021-01-13 16:00:08 +0000

    Created order domain

---
+++ src/main/java/com/qa/ims/persistence/domain/Orders.java
@@ -0,0 +1,106 @@
+package com.qa.ims.persistence.domain;
+
+import java.time.LocalDate;
+
+public class Orders {
+	private Long id;
+	private String OrderAddress;
+	private LocalDate OrderDate = LocalDate.now();
+	private Long CustomerID;
+
+	public Orders(String orderAddress, LocalDate orderDate, Long customerID) {
+		super();
+		OrderAddress = orderAddress;
+		OrderDate = orderDate;
+		CustomerID = customerID;
+	}
+
+	public Orders(Long id, String orderAddress, LocalDate orderDate, Long customerID) {
+		super();
+		this.id = id;
+		OrderAddress = orderAddress;
+		OrderDate = orderDate;
+		CustomerID = customerID;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getOrderAddress() {
+		return OrderAddress;
+	}
+
+	public void setOrderAddress(String orderAddress) {
+		OrderAddress = orderAddress;
+	}
+
+	public LocalDate getOrderDate() {
+		return OrderDate;
+	}
+
+	public void setOrderDate(LocalDate orderDate) {
+		OrderDate = orderDate;
+	}
+
+	public Long getCustomerID() {
+		return CustomerID;
+	}
+
+	public void setCustomerID(Long customerID) {
+		CustomerID = customerID;
+	}
+
+	@Override
+	public String toString() {
+		return "id:" + id + " OrderAddress:" + OrderAddress + " OrderDate:" + OrderDate + " CustomerID:" + CustomerID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((CustomerID == null) ? 0 : CustomerID.hashCode());
+		result = prime * result + ((OrderAddress == null) ? 0 : OrderAddress.hashCode());
+		result = prime * result + ((OrderDate == null) ? 0 : OrderDate.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Orders other = (Orders) obj;
+		if (CustomerID == null) {
+			if (other.CustomerID != null)
+				return false;
+		} else if (!CustomerID.equals(other.CustomerID))
+			return false;
+		if (OrderAddress == null) {
+			if (other.OrderAddress != null)
+				return false;
+		} else if (!OrderAddress.equals(other.OrderAddress))
+			return false;
+		if (OrderDate == null) {
+			if (other.OrderDate != null)
+				return false;
+		} else if (!OrderDate.equals(other.OrderDate))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,4 +1,4 @@
 create database if not exists ims;
 create table if not exists ims.customers(id int primary key auto_increment, first_name varchar(40), surname varchar(40));
 create table if not exists ims.items(id int primary key auto_increment, item_name varchar(40), Price double);
-create table if not exists ims.orders(id int primary key auto_increment, order_date date, customerid int, FOREIGN KEY (customerid) REFERENCES customers(id));
+create table if not exists ims.orders(id int primary key auto_increment, order_address varchar(40), order_date varchar(40), customerid int, FOREIGN KEY (customerid) REFERENCES customers(id));
